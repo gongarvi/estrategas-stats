533b148035f0c34e8130d3ef5fc058f6d1db95a4
Gongarvi <unaimarg@gmail.com>
2022-06-07 21:41:20 +0200

    redesign index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.master')
 @section('content')
-    <div class="">
-        <ul class="flex flex-wrap items-stretch justify-center dark:bg-gray-900 sm:items-center sm:pt-0">
+    <div class="h-screen flex place-items-center">
+        <ul class="px-0 sm:px-5 h-fit w-full flex flex-wrap place-items-center dark:bg-gray-900 gap-4">
             @foreach ($data as $item)
-                <li class="m-1">
-                    <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                        <div class="p-6">
-                            <h5 class="text-gray-900 text-xl font-medium mb-2">{{$item->name}}</h5>
-                            <a href="{{route('save', $item->key)}}" class="">
-                                Go
-                            </a>
+                <li class="m-auto">
+                    <a href="{{route('save', $item->key)}}" class="">
+                        <div class="rounded-lg shadow-lg bg-white w-40 h-40 hover:scale-105 hover:shadow-slate-600 hover:cursor-pointer">
+                            <div class="p-6 flex h-full place-items-center">
+                                <h5 class="w-full text-gray-900 text-xl text-center font-medium mb-2">{{$item->name}}</h5>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </li>
             @endforeach
         </ul>
